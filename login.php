40de89398df8ce8b9439128932599aca79dc9b16
Peace -M <gladys.mwatati@strathmore.edu>
2021-09-11 02:05:07 +0300

    added css

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ include "header.php";
         position: relative;
         margin: 100px auto;
         width: 370px;
-        height: 315px;
+        height: 360px;
         background: rgba(255, 255, 255, 0.05);
         backdrop-filter: blur(10px);
         border-top: 1px solid rgba(255, 255, 255, 0.2);
@@ -20,14 +20,26 @@ include "header.php";
 
     .botn {
         text-align: center;
+        color: black;
+    }
+    button{
+        background-color: black;
+        color: white;
+        width: 150px;
+        border-radius: 30px;
+    }
+    p{
+        font-family: sans-serif;
+        font-size: large;
     }
 </style>
 <section class="container ">
 
     <div class="signup">
         <form class="form-login" action="includes/login.inc.php" method="POST">
-            <h2 class="h2">Login</h2>
-
+        <i class="fas fa-users" style="font-size:120px;color:navy blue;padding:0px 0px 0px 100px "></i>
+        <br>
+            
 
             <label for="username">Enter your username :</label>
             <input type="text" class="form-control" name="username">
@@ -41,7 +53,7 @@ include "header.php";
                 <?php
                 if (isset($_GET["error"])) {
                     if ($_GET["error"] == "emptyinput") {
-                        echo "<p>fill in all fields!</p> ";
+                        echo "<p>Fill in all Fields!</p> ";
                     } else if ($_GET["error"] == "wronglogin") {
                         echo "<p>Incorrect Login details!</p> ";
                     }
--- stylist/signup.php
+++ stylist/signup.php
@@ -16,13 +16,18 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
-
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="">
 
     <title> Salon Operations Management</title>
 </head>
 <body>
+    <style type="text/css">
+
+
+    </style>
 <section class="container">
     <div>
     <form action="Includes/signup.inc.php" method="POST">
